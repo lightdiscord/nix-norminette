966b292098fadcb07c1e80d3cd7c0d4b2728ad1a
LightDiscord <root@arnaud.sh>
2021-03-09 14:59:58 +0100

    norminette: 3.1.3 -> 3.2.0

--- default.nix
+++ default.nix
@@ -2,21 +2,18 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "norminette";
-  version = "3.1.3";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "42School";
     repo = "norminette";
     rev = "${version}";
-    sha256 = "N42Xz2JQ6/Wx3kvde4o+QyxjUpuYE2gvt2BwCteqQfw=";
+    sha256 = "gNt4+jV4MYPVy1gx5ys18yJWpMKi65luTD/v8TkuCcQ=";
   };
 
   patchPhase = ''
     # Since python 3.2, argparse is part of the python standard library.
-    echo > requirements.txt
-
-    # And, is it really necessary to run pip install inside setup.py ?
-    sed -i '/pip install -r requirements.txt/d' setup.py
+    sed -i '/argparse/d' requirements.txt setup.cfg
   '';
 
   # Needed because they need to import pkg_resources.
