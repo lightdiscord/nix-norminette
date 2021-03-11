5893a428bccfcc01c4fe19b115ed3b0bbf60173a
LightDiscord <root@arnaud.sh>
2021-03-11 15:07:35 +0100

    norminette: 3.2.0 -> 3.2.1

--- default.nix
+++ default.nix
@@ -2,22 +2,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "norminette";
-  version = "3.2.0";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
     owner = "42School";
     repo = "norminette";
     rev = "${version}";
-    sha256 = "gNt4+jV4MYPVy1gx5ys18yJWpMKi65luTD/v8TkuCcQ=";
+    sha256 = "o3VMMRJg4JSQVw6VkoWb7bOmrT/b0WTiW0f9Zb7r5gM=";
   };
-
-  patchPhase = ''
-    # Since python 3.2, argparse is part of the python standard library.
-    sed -i '/argparse/d' requirements.txt setup.cfg
-  '';
-
-  # Needed because they need to import pkg_resources.
-  propagatedBuildInputs = [ python3Packages.setuptools ];
 
   doCheck = false;
 }
