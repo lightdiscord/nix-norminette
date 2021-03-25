2a0eab57ff89f05c9ff6b345ad764dddea9df8c7
LightDiscord <root@arnaud.sh>
2021-03-25 22:59:37 +0100

    norminette: 3.2.1 -> 3.2.3

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "norminette";
-  version = "3.2.1";
+  version = "3.2.3";
 
   src = fetchFromGitHub {
     owner = "42School";
     repo = "norminette";
     rev = "${version}";
-    sha256 = "o3VMMRJg4JSQVw6VkoWb7bOmrT/b0WTiW0f9Zb7r5gM=";
+    sha256 = "uYQ7UpOWwPosAJGQzFRNcbAZ4Lcu8CyKN9p9v+crcyE=";
   };
 
   doCheck = false;
